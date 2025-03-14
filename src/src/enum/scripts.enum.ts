35f80912a1d4af6aaf28005e740aee8a45e703c0
Rudolf Grabowski <grabas01@gmail.com>
2025-03-14 23:28:32 +0100

    rename folder

--- src/src/enum/scripts.enum.ts
+++ src/src/enum/scripts.enum.ts
@@ -1,6 +1,6 @@
 export const ScriptsEnum = {
-    GROW_BATCH: { path: "src/virus/growBatch.js", size: 1.75 },
-    WEAKEN_BATCH: { path: "src/virus/weakenBatch.js", size: 1.75 },
-    HACK_BATCH: { path: "src/virus/hackBatch.js", size: 1.70 },
-    PREPARATOR: { path: "src/virus/preparator.js", size: 2.1 }
+    GROW_BATCH: { path: "src/hacking/growBatch.js", size: 1.75 },
+    WEAKEN_BATCH: { path: "src/hacking/weakenBatch.js", size: 1.75 },
+    HACK_BATCH: { path: "src/hacking/hackBatch.js", size: 1.70 },
+    PREPARATOR: { path: "src/hacking/preparator.js", size: 2.1 }
 }
